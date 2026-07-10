4651ffa31e50d15e0be027a357aebdbe65734f34
Sujit-Thapa <sujitthapa.professional@gmail.com>
2026-07-10 21:37:04 +0545

    refactor(page.tsx): update Footer import path for consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Footer from "@/components/footer";
+import Footer from "../components/footer";
 
 const menuSections = [
   {
